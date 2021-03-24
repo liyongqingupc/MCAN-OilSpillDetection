1a4c59942a72f6d76372898425d932d3ee1b1ae9
Yongqing Li <53959630+liyongqingupc@users.noreply.github.com>
2021-03-24 20:29:26 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from config import get_arguments
 from MCAN.functions import *
 import matplotlib.pyplot as plt
 import numpy as np
-import torch.nn as nn
 from skimage import io as img
 from MCAN.imresize import imresize
 
@@ -20,94 +19,57 @@ opt = functions.post_config(opt)
 
 # input the folder name of trained model
 trained_model = opt.trained_model
+
 ########################################
 # Gs
 pthfile1 = r'TrainedModels/%s/Gs.pth' % trained_model
 Gs = torch.load(pthfile1)
 
-# NoiseAmp
-pthfile3 = r'TrainedModels/%s/NoiseAmp.pth' % trained_model
-NoiseAmp = torch.load(pthfile3)
-
-# real2s
-pthfile4 = r'TrainedModels/%s/real2s.pth' % trained_model
-real2s = torch.load(pthfile4)
-
-for test_num in range(1,31,1):  # the index of test images
+for test_num in range(5, 31, 1):  # the index of test images
     opt.input_name = 'test' + str(test_num) + '.jpg'   # the name of test image
     x = img.imread('%s/%s' % (opt.input_dir, opt.input_name))  # img.imread
     x = np2torch(x, opt)
+
     x = x[:, 0:3, :, :]
     a = x.shape
     b = int(a[3])
-    real1_ = x[:, 0:3, :, 0:int(b / 2)]  # left image
-    real1_ = (real1_ - real1_.min()) / (real1_.max() - real1_.min())  # normalize()
+    real1_ = x[:, 0:3, :, 0:int(b / 2)]  # extract left image
 
-    real2_ = x[:, 0:3, :, int(b / 2):b]  # right image
-    #real2_ = (real2_ - real2_.min()) / (real2_.max() - real2_.min())  # normalize()
+    ###### normalize() -> [0,1] ######
+    real1_ = (real1_ - real1_.min()) / (real1_.max() - real1_.min())
 
     functions.adjust_scales2image(real1_, opt)
-    real1 = imresize(real1_, opt.scale1, opt)
+    real1 = imresize(real1_, 0, opt)
     real1s = []
     real1s = functions.creat_reals_pyramid(real1, real1s, opt)
 
-    in_s = None
-    scale_v = 1
-    scale_h = 1
-    n = 0  # floor num
-    gen_start_scale = 0  # gen_start_scale
-    num_samples = 1  # num_samples: num of generated image for each scale
+    images_cur = None
+    n = 0  # scale num
+    
+    images_cur = torch.full(real1s[0].shape, 0, device = opt.device)  # images_cur
 
-
-    if in_s is None:
-        in_s = torch.full(real2s[0].shape, 0, device=opt.device)  # creat 0 matrix
-    images_cur = []  # images_cur
-
-    for G, Z_opt, noise_amp in zip(Gs, real1s, NoiseAmp):
-        pad1 = ((opt.ker_size - 1) * opt.num_layer) / 2  # pad1  (2*5)/2=5
-        m = nn.ZeroPad2d(int(pad1))  # padding
-
+    for G, real_in in zip(Gs, real1s):  # use each trained G
         images_prev = images_cur  # set images_prev
-        images_cur = []  # set images_cur as [],
 
-        for i in range(0, num_samples, 1):  # num_samples =1 : run 1 loop
-            if n == 0:  # n == 0 is the coarsest scale
-                opt.nzx = real1s[n].shape[2]
-                opt.nzy = real1s[n].shape[3]
-                z_curr = real1s[n].expand(1, 3, opt.nzx, opt.nzy)
-                z_curr = m(z_curr)
-            else:
-                opt.nzx = real1s[n].shape[2]
-                opt.nzy = real1s[n].shape[3]
-                z_curr = real1s[n].expand(1, opt.nc_z, opt.nzx, opt.nzy)
-                z_curr = m(z_curr)
+        #print('images_prev:', images_prev.shape)
+        #print('real_in:', real_in.shape)
+        g_in = torch.cat((real_in, images_prev),1).detach()  # input of G
+        I_curr = G(g_in, images_prev)  # output of G
 
-            if images_prev == []:
-                I_prev = m(in_s)  # now, in_s is 0 matrix
-            else:
-                I_prev = images_prev[i]
-                I_prev = imresize(I_prev, 1/opt.scale_factor, opt)
-                if opt.mode == "test":
-                    I_prev = I_prev[:, :, 0:round(scale_v * real2s[n].shape[2]), 0:round(scale_h * real2s[n].shape[3])]
-                    I_prev = m(I_prev)  # m
-                    I_prev = I_prev[:, :, 0:z_curr.shape[2], 0:z_curr.shape[3]]
-                    I_prev = functions.upsampling(I_prev, z_curr.shape[2], z_curr.shape[3])  # I_prev upsampling
-                else:
-                    I_prev = m(I_prev)
+        images_cur = []  # set as []
+        images_cur = I_curr
+        images_cur = imresize(images_cur, -1, opt)  # upsample
 
-            if n < gen_start_scale:
-                z_curr = Z_opt
+        ############################### SET opt.scale_num #######################################
+        if n == opt.scale_num:  # reach the finest scale
 
-            z_in = noise_amp * (z_curr) + I_prev  # input
-            I_curr = G(z_in.detach(), I_prev)  # output
-            ################## output --> 1 ##################
-            I_curr = (I_curr - I_curr.min())/(I_curr.max() - I_curr.min())  # normalize()
-            I_cu = functions.convert_image_np(I_curr.detach())
+            for threshold in range(70, 74, 2):   #
+                ################## output normalize()--> 1 ###################
+                I_curr = (I_curr - I_curr.min()) / (I_curr.max() - I_curr.min())
 
-            if n == len(real2s) - 1:  # reach at finest scale
-                threshold = 0.9  # Set the threshold of output
-                I_cu[np.where(I_cu <= threshold)] = 0
-                I_cu[np.where(I_cu > threshold)] = 1
+                I_cu = functions.convert_image_np(I_curr.detach())   # function ?????????
+
+                I_cu = (I_cu[:, :, 0] + I_cu[:, :, 1] + I_cu[:, :, 2]) / 3  # 3D --> 2D lyq210118
 
                 if opt.mode == 'test':
                     dir2save = '%s/TestResult_%s' % (opt.out, trained_model)
@@ -115,9 +77,17 @@ for test_num in range(1,31,1):  # the index of test images
                     os.makedirs(dir2save)
                 except OSError:
                     pass
+                ####################### POW ###########################
+                I_save = pow(I_cu, 1)  #
+                #print('I_save.shape:', I_save.shape, 'I_save',I_save)  # 256*256
+                #print('I_cu.shape:', I_cu.shape, 'I_cu', I_cu)
+                I_save[np.where(I_save <= float(threshold/100))] = 0
+                I_save[np.where(I_save > float(threshold/100))] = 255
+
                 # save the generated image
-                plt.imsave('%s/%s' % (dir2save, opt.input_name), I_cu, vmin=0, vmax=1)
-            images_cur.append(I_curr)  # images_cur has been cleared above
+                opt.save_name = 'test' + str(test_num) + '_' + str(float(threshold/100)) + '.jpg'  # the name of test image
+                plt.imsave('%s/%s' % (dir2save, opt.save_name), I_save, cmap='gray', vmin=0, vmax=1)
+
 
         n += 1  #  n + 1, enter next scale
 
---
+++ training.py
@@ -0,0 +1,197 @@
+import MCAN.functions as functions
+import MCAN.models as models
+import os
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+import math
+import matplotlib.pyplot as plt
+from MCAN.imresize import imresize
+import numpy as np
+
+def train(opt,Gs,Ds,real1s,real2s,train_range,train_index,train_i,train_loopnum):
+    in_s = 0
+    scale_num = 0
+
+    opt.input_name = 'train' + str(train_range[train_index]) + '.jpg'  # the name of training image
+    real1_, real2_ = functions.read_image(opt)   #read_image
+
+    real1 = imresize(real1_, 0, opt)  #opt.scale1->opt.scale_num->0
+    real2 = imresize(real2_, 0, opt)  #opt.scale1->opt.scale_num->0
+    #print('real2:', real2.shape)  #[1, 3, 256, 256]
+    real1s = functions.creat_reals_pyramid(real1, real1s, opt)
+    real2s = functions.creat_reals_pyramid(real2, real2s, opt)
+
+    while scale_num < opt.scale_num + 1:  # opt.stop_scale->opt.scale_num
+        opt.nfc = min(opt.nfc_init * pow(2, math.floor(scale_num / 4)), 128)  # adjust the num of kernel
+        opt.min_nfc = min(opt.min_nfc_init * pow(2, math.floor(scale_num / 4)), 128)
+        opt.out_ = functions.generate_dir2save(opt)
+        opt.outf = '%s/%d' % (opt.out_, scale_num)  # output path, used in functions.save_networks(netG,netD,opt)
+        try:
+            os.makedirs(opt.outf)
+        except OSError:
+            pass
+
+        if train_i == 0:  # if it is the first image
+            D_curr, G_curr = init_models(opt)  # init_models()
+        else:
+            D_curr, G_curr = init_models(opt)
+            G_curr.load_state_dict(torch.load('%s/%d/netG.pth' % (opt.out_, scale_num)))  # load the previous model
+            D_curr.load_state_dict(torch.load('%s/%d/netD.pth' % (opt.out_, scale_num)))
+
+        # train_single_scale
+        in_s, G_curr, D_curr = train_single_scale(
+                               D_curr, G_curr, real1s, real2s, Gs, in_s, opt, train_index, train_i, train_loopnum, scale_num)  # reals -- real1s,real2s  # lyq add D_cu
+
+        G_curr = functions.reset_grads(G_curr, False)  # reset_grads
+        G_curr.eval()                                  # eval()
+        D_curr = functions.reset_grads(D_curr, False)
+        D_curr.eval()
+
+        Gs.append(G_curr)  # Gs
+        Ds.append(D_curr)  # Ds
+
+        scale_num += 1  # enter next scale
+
+        del D_curr, G_curr  # delete current model, creat new model at next scale
+
+    # saved in TrainedModels
+    torch.save(Gs, '%s/Gs.pth' % (opt.out_))
+    torch.save(Ds, '%s/Ds.pth' % (opt.out_))   #
+    torch.save(real2s, '%s/real2s.pth' % (opt.out_))  #
+    torch.save(real1s, '%s/real1s.pth' % (opt.out_))  #
+
+    return
+
+####################################
+###    train_single_scale       ###
+####################################
+def train_single_scale(netD,netG,real1s,real2s,Gs,in_s,opt,train_index,train_i, train_loopnum, scale_num):
+
+    #print('train_index:', train_index)
+    real2 = real2s[len(Gs)]  #get the current image from reals, len(Gs) is index
+    real1 = real1s[len(Gs)]
+
+    opt.nzx = real2.shape[2]  #real --real2
+    opt.nzy = real2.shape[3]  #real --real2
+    #print('opt.nzx:', opt.nzx, 'opt.nzy:', opt.nzy)  # lyq # 64,64
+
+    alpha = opt.alpha  # alpha
+
+    # setup optimizer
+    optimizerD = optim.Adam(netD.parameters(), lr=opt.lr_d, betas=(opt.beta1, opt.beta2)) # netD.parameters()
+    optimizerG = optim.Adam(netG.parameters(), lr=opt.lr_g, betas=(opt.beta1, opt.beta2))  #lyq add beta2
+    schedulerD = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerD,milestones=[1600],gamma=opt.gamma)
+    schedulerG = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizerG,milestones=[1600],gamma=opt.gamma) #gamma=0.1
+
+    errD2plot = []
+    errG2plot = []
+    D_real2plot = []
+    D_fake2plot = []
+
+    if scale_num == opt.scale_num:  #lyq 0317
+        niter_num = 1  # finest scale train 1 time
+    else:
+        niter_num = opt.niter # other scale train opt.niter time
+
+    for epoch in range(niter_num):
+        ############################
+        # (1) Update D network: minimize -D(x) + D(G(z))
+        ###########################
+        for j in range(opt.Dsteps): # The num of training D
+            # train with real
+            netD.zero_grad()  # zero_grad:
+            output = netD(torch.cat((real1, real2), 1)).to(opt.device)  # input image pairs
+            errD_real = -output.mean()  #-a
+            errD_real.backward(retain_graph=True)  # retain_graph=True
+            D_x = -errD_real.item()
+
+            if (Gs == []): # lyq add 0315  Ok or Not ????
+                prev = torch.full([1,opt.nc_z,opt.nzx,opt.nzy], 0, device=opt.device)  # full:generete 0 matrix
+            else:
+                #print('real1_prev:',real1_prev.shape, 'in_s:',in_s.shape)
+                prev = in_s
+            print('real1:',real1.shape, 'prev:',prev.shape)  #Dsteps=3 has error
+            fake = netG(torch.cat((real1, prev),1).detach(), prev) # lyq add 0315
+
+            output = netD(torch.cat((real1, fake),1).detach())   # lyq add torch.cat 0911 # .detach() is Ok ????
+            errD_fake = output.mean()
+            errD_fake.backward(retain_graph=True)
+            D_G_z = output.mean().item()
+
+            # calc_gradient_penalty
+            gradient_penalty = functions.calc_gradient_penalty(netD, real1, real2, fake, opt.lambda_grad, opt.device)
+            gradient_penalty.backward()
+
+            ######## errD: fake - real + gp ########
+            errD = errD_real + errD_fake + gradient_penalty   ### The errD ###
+
+            if train_i % 20 == 0 and epoch % 10 == 0:
+                print('errD:', errD)
+                print('gradient_penalty:', gradient_penalty)
+
+            optimizerD.step()   # optimizerD
+
+        errD2plot.append(errD.detach())  # errD: tensor(1.8132, device='cuda:0', grad_fn=<AddBackward0>)
+
+        ############################
+        # (2) Update G network:
+        ###########################
+        for j in range(opt.Gsteps):
+            netG.zero_grad()
+            output = netD(torch.cat((real1, fake), 1))
+            errG = -output.mean()
+            errG.backward(retain_graph=True)
+
+            if alpha != 0:
+                gen_image = fake  # lyq add 0315
+                norm_loss = alpha * l1(gen_image, real2)  #
+                norm_loss.backward(retain_graph=True)   # lyq add
+                if train_i % 20 == 0 and epoch % 10 == 0:
+                    print('errG:', errG)
+                    print('norm_loss:', norm_loss)
+                norm_loss = norm_loss.detach()   # lyq add
+
+            optimizerG.step()  # optimizerG
+
+        errG2plot.append(errG.detach() + norm_loss)
+
+        D_real2plot.append(D_x)
+        D_fake2plot.append(D_G_z)
+
+        if train_i % 20 == 0 and epoch % 10 == 0:
+            print('train_i:[%d/%d]-train_index:%d-scale:%d-iteration:[%d/%d]' % (train_i+1,train_loopnum,train_index,len(Gs), epoch+1, opt.niter))
+
+        if train_i % 10 == 0 and epoch % 10 == 0:
+            plt.imsave('%s/fake_sample.png' % (opt.outf), functions.convert_image_np(fake.detach()), vmin=0, vmax=1)
+            plt.imsave('%s/z_prev.png' % (opt.outf), functions.convert_image_np(prev), vmin=0, vmax=1)
+
+        schedulerD.step()
+        schedulerG.step()
+
+    functions.save_networks(netG, netD, opt)   #save_networks
+    in_s = imresize(fake.detach(), -1, opt) # in_s is prepared for the next scale # +.detach()  # lyq 0317
+
+    return in_s, netG, netD    # return z_opt,in_s,netG  #lyq delete z_opt
+
+
+def l2(x, y):
+    return (torch.pow(x - y, 2)).mean()  # lyq add
+
+def l1(x, y):
+    return (torch.abs(x - y)).mean()  # lyq add
+
+
+def init_models(opt):
+    #generator initialization:
+    netG = models.GeneratorConcatSkip2CleanAdd(opt).to(opt.device)
+    netG.apply(models.weights_init)
+    #print(netG)   #lyq #
+
+    #discriminator initialization:
+    netD = models.WDiscriminator(opt).to(opt.device)
+    netD.apply(models.weights_init)
+    if opt.netD != '':  #
+        netD.load_state_dict(torch.load(opt.netD))
+    #print(netD)   #lyq #
+    return netD, netG
